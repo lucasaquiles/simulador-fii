4c2bec3da42174329458f3a28fb25ceb0a1af8c6
Lucas Aquiles <lucasaquiles@gmail.com>
2020-06-25 16:06:23 -0300

    Update prod.env.js

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  VUE_APP_FII_SOURCE_URL: '"http://crawler-simiulador-fii.netlify.com"'
+  VUE_APP_FII_SOURCE_URL: '"https://fii-crawler-api.herokuapp.com/stocks/sdil11"'
 }
