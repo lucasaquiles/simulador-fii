5f7d18d97da63c391585d6b6fa40abe00bbe12e1
Lucas Aquiles <lucasaquiles@gmail.com>
2020-06-27 18:44:05 -0300

    correção da URL de dev

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  VUE_APP_FII_SOURCE_URL: '"http:localhost:8081/"'
+  VUE_APP_FII_SOURCE_URL: '"http://localhost:8081/"'
 })
